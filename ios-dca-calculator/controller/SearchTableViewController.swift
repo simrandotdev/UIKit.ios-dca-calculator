8088f0ac97370db69c4f65b23e2e302a2057d709
Simran Preet Singh <simran5590@gmail.com>
2022-12-22 22:54:45 +0530

    Add search controller

--- ios-dca-calculator/controller/SearchTableViewController.swift
+++ ios-dca-calculator/controller/SearchTableViewController.swift
@@ -10,7 +10,55 @@ import UIKit
 
 class SearchTableViewController: UITableViewController {
     
+    // MARK: UI Properties
+    private lazy var searchController: UISearchController = {
+        let sc = UISearchController(searchResultsController: nil)
+        sc.searchResultsUpdater = self
+        sc.delegate = self
+        sc.obscuresBackgroundDuringPresentation = false
+        sc.searchBar.placeholder = "Enter a company name or symbol"
+        sc.searchBar.autocapitalizationType = .allCharacters
+        return sc
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTitle()
+        setupNavigationBar()
     }
+}
+
+
+// MARK: - UI Setups
+
+
+extension SearchTableViewController {
+    
+    func setupTitle() {
+        title = "DCA Calculator"
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.searchController = searchController
+    }
+}
+
+
+// MARK: - UISearchResultsUpdating
+
+
+extension SearchTableViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+}
+
+
+// MARK: - UISearchControllerDelegate
+
+
+extension SearchTableViewController: UISearchControllerDelegate {
+    
 }
